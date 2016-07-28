133350735b1c7404e7480bc8a5c3fadaf5dd94ce
tidiview <francois.vidit@laposte.net>
2016-07-28 22:57:45 +0200

    unecessary file rmove

--- id/index-old.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-type:application/ld+json");
-
-// It will be called downloaded.pdf
-header("Content-Disposition:attachment;filename='id.jsonld'");
-
-// The PDF source is in original.pdf
-readfile("id.jsonld");
-?>
